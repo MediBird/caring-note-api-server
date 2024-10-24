e78dbb3161051be5bbc49ceaea70426b211ccd4d
choisunpil <roung4119@gmail.com>
2024-10-25 00:52:47 +0900

    [Chore] 불필요한 import 제거

--- src/main/java/com/springboot/api/config/security/SecurityConfig.java
+++ src/main/java/com/springboot/api/config/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.springboot.api.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
