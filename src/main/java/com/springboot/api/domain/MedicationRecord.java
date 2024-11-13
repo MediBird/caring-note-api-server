3050cf40c3bbf67b5c681f5b22a71e88e407d965
Sanghoon <bshjaws1@gmail.com>
2024-11-13 20:46:39 +0900

    feat: :sparkles: add medicationRecord

---
+++ src/main/java/com/springboot/api/domain/MedicationRecord.java
@@ -0,0 +1,78 @@
+package com.springboot.api.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+public class MedicationRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 환자 엔티티와의 관계 매핑 (다대일)
+    @ManyToOne
+    @JoinColumn(name = "counselee_id", nullable = false)
+    private Patient patient;
+
+    // 의약품 엔티티와의 관계 매핑 (다대일)
+    @ManyToOne
+    @JoinColumn(name = "medication_id", nullable = false)
+    private Medication medication;
+
+    private String dosage;
+
+    private LocalDateTime administrationDateTime;
+
+    private String routeOfAdministration;
+
+    // 의사 엔티티와의 관계 매핑 (옵션)
+    @ManyToOne
+    @JoinColumn(name = "prescribing_counselor_id")
+    private Counselor prescribingCounselor;
+
+    private String notes;
+
+    @Enumerated(EnumType.STRING)
+    private AdministrationStatus status;
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDate prescriptionDate;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDate expiryDate;
+
+    @Min(0)
+    private int remainingQuantity;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+    
+}
