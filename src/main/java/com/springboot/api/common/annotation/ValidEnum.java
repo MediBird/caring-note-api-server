56bf8cc7b8c55024cc6177fad8fd365901a3d6de
choisunpil <roung4119@gmail.com>
2024-11-24 22:12:18 +0900

    feat: ✨add ValidEnum Annotation

---
+++ src/main/java/com/springboot/api/common/annotation/ValidEnum.java
@@ -0,0 +1,18 @@
+package com.springboot.api.common.annotation;
+
+import com.springboot.api.common.validator.EnumValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+    String message() default "Invalid value for enum";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
---
+++ src/main/java/com/springboot/api/common/validator/EnumValidator.java
@@ -0,0 +1,26 @@
+package com.springboot.api.common.validator;
+
+import com.springboot.api.common.annotation.ValidEnum;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.stream.Stream;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum<?>> {
+
+    private Class<? extends Enum<?>> enumClass;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.enumClass = constraintAnnotation.enumClass();
+    }
+
+    @Override
+    public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false; // 값이 null이면 유효하지 않음
+        }
+        return Stream.of(enumClass.getEnumConstants())
+                .anyMatch(enumValue -> enumValue.name().equals(value.name()));
+    }
+}
