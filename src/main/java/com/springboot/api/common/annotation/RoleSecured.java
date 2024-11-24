cc1080fbbba6ec3bf7b7966256ad2301a0b3c6c4
choisunpil <roung4119@gmail.com>
2024-11-24 20:34:12 +0900

    refactor: ♻️modify RoleSecured Annotation using RoleType

--- src/main/java/com/springboot/api/common/annotation/RoleSecured.java
+++ src/main/java/com/springboot/api/common/annotation/RoleSecured.java
@@ -1,5 +1,7 @@
 package com.springboot.api.common.annotation;
 
+import com.springboot.enums.RoleType;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE}) // 클래스나 메서드에 적용
 @Retention(RetentionPolicy.RUNTIME) // 런타임에 유지
 public @interface RoleSecured {
-    String[] value(); // 적용할 권한을 지정할 수 있도록
+    RoleType[] value(); // 적용할 권한을 지정할 수 있도록
 }
