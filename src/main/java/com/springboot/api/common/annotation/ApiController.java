1748a8f33e40a2ae92190d932f051e4e33c4546c
choisunpil <roung4119@gmail.com>
2024-11-30 16:32:27 +0900

    feat: ✨ add ApiController Annotation for unifying @RestController, @RequestMapping, @Tag

---
+++ src/main/java/com/springboot/api/common/annotation/ApiController.java
@@ -0,0 +1,27 @@
+package com.springboot.api.common.annotation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@RestController
+@RequestMapping
+@Tag(name = "")
+public @interface ApiController {
+    @AliasFor(annotation = RequestMapping.class, attribute = "value")
+    String path();
+
+    @AliasFor(annotation = Tag.class, attribute = "name")
+    String name();
+
+    @AliasFor(annotation = Tag.class, attribute = "description")
+    String description();
+}
