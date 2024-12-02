46ce724ed2d7b11df476a84e857d7a2451421b3a
Sanghoon <bshjaws1@gmail.com>
2024-12-03 01:49:04 +0900

    test: :white_check_mark: add test code and resolve errors

--- src/main/java/com/springboot/api/repository/CounselCardRepository.java
+++ src/main/java/com/springboot/api/repository/CounselCardRepository.java
@@ -1,19 +1,17 @@
 package com.springboot.api.repository;
 
-
-import com.springboot.api.domain.CounselCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
-public interface CounselCardRepository extends JpaRepository<CounselCard, String> {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.springboot.api.domain.CounselCard;
+
+public interface CounselCardRepository extends JpaRepository<CounselCard, String> {
 
     CounselCard save(CounselCard counselCard);
 
     Optional<CounselCard> findById(String id);
 
     void deleteById(String id);
-
 
 }
--- src/test/java/com/springboot/api/controller/CounselorControllerTest.java
+++ src/test/java/com/springboot/api/controller/CounselorControllerTest.java
@@ -1,25 +1,36 @@
 package com.springboot.api.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.springboot.api.dto.counselor.AddCounselorReq;
-import com.springboot.api.dto.counselor.LoginCounselorReq;
-import com.springboot.enums.RoleType;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.api.common.util.JwtUtil;
+import com.springboot.api.dto.counselor.AddCounselorReq;
+import com.springboot.api.dto.counselor.AddCounselorRes;
+import com.springboot.api.dto.counselor.LoginCounselorReq;
+import com.springboot.api.dto.counselor.LoginCounselorRes;
+import com.springboot.api.service.CounselorService;
+import com.springboot.enums.RoleType;
+
 @AutoConfigureMockMvc
+@SpringBootTest
 public class CounselorControllerTest {
 
     @Autowired
@@ -28,8 +39,21 @@ public class CounselorControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @MockBean
+    private CounselorService counselorService;
+
     @Test
     void shouldAddCounselorSuccessfully() throws Exception {
+        Mockito.when(counselorService.addCounselor(any(AddCounselorReq.class)))
+                .thenReturn(new AddCounselorRes("01HXBK6P4NCZXCSNAG3FY7YZDT", RoleType.ADMIN));
+
+        Mockito.when(jwtUtil.createTokenResponse(eq("01HXBK6P4NCZXCSNAG3FY7YZDT"), eq(RoleType.ADMIN)))
+                .thenReturn(ResponseEntity.status(HttpStatus.CREATED)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer mock-token")
+                        .build());
 
         AddCounselorReq request = AddCounselorReq.builder()
                 .name("sunpil")
@@ -40,24 +64,30 @@ public class CounselorControllerTest {
                 .build();
 
         // When: 요청 수행
-        mockMvc.perform(post("/api/v1/counselor/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                // Then: 응답 검증
+        mockMvc.perform(post("/v1/counselor/signup").with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andDo(result -> {
+                    System.out.println("Response Status: " + result.getResponse().getStatus());
+                    System.out.println("Response Headers: " + result.getResponse().getHeaderNames());
+                })
                 .andExpect(status().isCreated())
                 .andExpect(header().string(HttpHeaders.AUTHORIZATION, startsWith("Bearer ")));
-
-
     }
 
-
-
-
     @Test
-    @Transactional
     void shouldAddCounselorAndLoginSuccessfully() throws Exception {
-        // Given: 요청 데이터 준비
-        AddCounselorReq request = AddCounselorReq.builder()
+        Mockito.when(counselorService.addCounselor(any(AddCounselorReq.class)))
+                .thenReturn(new AddCounselorRes("01HXBK6P4NCZXCSNAG3FY7YZDT", RoleType.ADMIN));
+
+        Mockito.when(jwtUtil.createTokenResponse(eq("01HXBK6P4NCZXCSNAG3FY7YZDT"), eq(RoleType.ADMIN)))
+                .thenReturn(ResponseEntity.status(HttpStatus.CREATED)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer mock-token")
+                        .build());
+        Mockito.when(counselorService.loginCounselor(any(LoginCounselorReq.class)))
+                .thenReturn(new LoginCounselorRes("01HXBK6P4NCZXCSNAG3FY7YZDT", RoleType.ADMIN));
+
+        AddCounselorReq signupRequest = AddCounselorReq.builder()
                 .name("sunpil.choi")
                 .email("roung4118@gmail.com")
                 .phoneNumber("01090664118")
@@ -70,21 +100,20 @@ public class CounselorControllerTest {
                 .password("12341234")
                 .build();
 
-        // When: 요청 수행
-        mockMvc.perform(post("/api/v1/counselor/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
+        // When: Counselor Signup
+        mockMvc.perform(post("/v1/counselor/signup").with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signupRequest)))
                 // Then: 응답 검증
                 .andExpect(status().isCreated())
                 .andExpect(header().string(HttpHeaders.AUTHORIZATION, startsWith("Bearer ")));
 
-
-        mockMvc.perform(post("/api/v1/counselor/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(loginRequest)))
+        // When: Counselor Login
+        mockMvc.perform(post("/v1/counselor/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginRequest)))
                 // Then: 응답 검증
                 .andExpect(status().isCreated())
                 .andExpect(header().string(HttpHeaders.AUTHORIZATION, startsWith("Bearer ")));
     }
-
 }
