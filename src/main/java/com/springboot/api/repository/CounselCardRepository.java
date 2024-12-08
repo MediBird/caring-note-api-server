ccefd8342fdaaacf5613c757dc94bbea75b9c73a
choisunpil <roung4119@gmail.com>
2024-12-08 23:57:29 +0900

    refactor: Remove redundant method declarations in Repositories

--- src/main/java/com/springboot/api/repository/CounselCardRepository.java
+++ src/main/java/com/springboot/api/repository/CounselCardRepository.java
@@ -1,17 +1,9 @@
 package com.springboot.api.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.springboot.api.domain.CounselCard;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CounselCardRepository extends JpaRepository<CounselCard, String> {
 
-    CounselCard save(CounselCard counselCard);
-
-    Optional<CounselCard> findById(String id);
-
-    void deleteById(String id);
 
 }
---
+++ src/main/java/com/springboot/api/repository/MedicationCounselRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.api.repository;
+
+import com.springboot.api.domain.MedicationCounsel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicationCounselRepository extends JpaRepository<MedicationCounsel, String> {
+
+
+}
