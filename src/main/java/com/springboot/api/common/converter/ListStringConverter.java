290b72532d8797a6bbb91bd11873cf602b8ad3ca
choisunpil <roung4119@gmail.com>
2024-12-16 23:14:29 +0900

    refactor: modify class Name ListToStringConverter -> ListStringConverter

--- src/main/java/com/springboot/api/common/converter/ListStringConverter.java
+++ src/main/java/com/springboot/api/common/converter/ListStringConverter.java
@@ -5,18 +5,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.api.common.exception.JsonConvertException;
 import jakarta.persistence.AttributeConverter;
-import org.springframework.stereotype.Component;
+import jakarta.persistence.Converter;
 
 import java.util.List;
 
-@Component
-public class ListToStringConverter implements AttributeConverter<List<String>, String> {
+@Converter
+public class ListStringConverter implements AttributeConverter<List<String>, String> {
 
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public ListToStringConverter(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     @Override
     public String convertToDatabaseColumn(List<String> attribute) throws RuntimeException {
