9a1dd946412acb3b717771ebde98d6df87beadcc
choisunpil <roung4119@gmail.com>
2024-12-16 23:17:17 +0900

    test: counseleeService.selectByCounselSessionId

---
+++ src/test/java/com/springboot/api/service/CounseleeServiceTest.java
@@ -0,0 +1,46 @@
+package com.springboot.api.service;
+
+
+import com.springboot.api.dto.counselee.SelectByCounselSessionIdRes;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class CounseleeServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(CounseleeServiceTest.class);
+    @Autowired
+    private CounseleeService counseleeService;
+
+
+    @Test
+    @Sql({
+            "/sql/test-counselee.sql"
+            ,"/sql/test-counselor.sql"
+            ,"/sql/test-counsel-session.sql"
+            ,"/sql/test-counsel-card.sql"
+    })
+    public void selectByCounselSessionId() {
+
+        String counselorId = "TEST-COUNSELOR-01";
+        String counselSessionId ="TEST-COUNSEL-SESSION-01";
+        String counseleeId ="TEST-COUNSELEE-01";
+
+        SelectByCounselSessionIdRes selectByCounselSessionIdRes =  counseleeService.selectByCounselSessionId(
+                counselorId
+                ,counselSessionId
+                ,counseleeId
+        );
+
+        log.debug(selectByCounselSessionIdRes.toString());
+
+    }
+
+
+}
