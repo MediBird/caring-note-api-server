a577a3579052b4aa5d5339682e0e2aa3561bc511
choisunpil <roung4119@gmail.com>
2024-12-30 22:13:44 +0900

    refactor: add jackson config

--- src/test/java/com/springboot/api/config/JpaTestAdditionalConfig.java
+++ src/test/java/com/springboot/api/config/JpaTestAdditionalConfig.java
@@ -1,5 +1,8 @@
 package com.springboot.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +15,12 @@ public class JpaTestAdditionalConfig {
         return entityManager;
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());  // Java 8 날짜/시간 모듈 등록
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);  // 타임스탬프 형식 출력 안 함
+            return objectMapper;
+        }
 }
 
