6e1a2a6586ce0c3db93b391fb8b68c66cc0735fd
choisunpil <roung4119@gmail.com>
2025-01-04 22:34:02 +0900

    refactor: add @Builder

--- src/main/java/com/springboot/api/domain/MedicationRecordHist.java
+++ src/main/java/com/springboot/api/domain/MedicationRecordHist.java
@@ -1,26 +1,20 @@
 package com.springboot.api.domain;
 
-import java.time.LocalDate;
-
 import com.springboot.enums.MedicationDivision;
 import com.springboot.enums.MedicationUsageStatus;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "medication_records_hist")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true, exclude = {"counselSession", "medication"})
 @ToString(callSuper = true, exclude = {"counselSession", "medication"})
 public class MedicationRecordHist extends BaseEntity {
