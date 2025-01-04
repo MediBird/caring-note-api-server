d787a4f9288c18623a50db6922a14c6e4354ea22
Sanghoon <bshjaws1@gmail.com>
2025-01-05 01:00:42 +0900

    feat: :wrench: add keycloak

--- src/main/java/com/springboot/api/common/config/security/SecurityConfig.java
+++ src/main/java/com/springboot/api/common/config/security/SecurityConfig.java
@@ -7,18 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.springboot.api.common.converter.CustomJwtRoleConverter;
-import com.springboot.api.repository.CounselorRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,36 +26,34 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityConfig {
 
     private final SecurityProperties securityProperties;
-    private final UserDetailsService userDetailsService;
-    private final CounselorRepository counselorRepository;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final CustomJwtRoleConverter customJwtRoleConverter;
     private final JwtDecoder jwtDecoder;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository)
-            throws Exception {
-        return http.csrf(CsrfConfigurer::disable) // CSRF 비활성화
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                        securityProperties.getPermitAllUrls().toArray(String[]::new))
-                .permitAll() // 인증 없이 접근 가능
-                .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                .requestMatchers(securityProperties.getPermitAllUrls().toArray(String[]::new))
+                .permitAll()
+                .anyRequest().authenticated()
                 )
+                .oauth2Login(oauth2 -> {
+                })
                 .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.decoder(jwtDecoder).jwtAuthenticationConverter(jwtAuthenticationConverter())))
-                .authenticationProvider(jwtAuthenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .jwt(jwt -> jwt.decoder(jwtDecoder)
+                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                )
+                )
                 .build();
     }
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomJwtRoleConverter(userDetailsService, counselorRepository));
-
-        return jwtAuthenticationConverter;
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(customJwtRoleConverter);
+        return converter;
     }
 
     @Bean
