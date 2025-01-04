24df2121ed54162a424d20d41938b1e48d536220
Sanghoon <bshjaws1@gmail.com>
2025-01-04 21:58:46 +0900

    feat: :wrench: add keycloak configuration

--- src/main/java/com/springboot/api/common/config/security/SecurityConfig.java
+++ src/main/java/com/springboot/api/common/config/security/SecurityConfig.java
@@ -8,25 +8,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.springboot.api.common.converter.CustomJwtRoleConverter;
+import com.springboot.api.repository.CounselorRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationProvider jwtAuthProvider;
-    private final JwtAuthenticationFilter jwtAuthFilter;
-
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter,
-            JwtAuthenticationProvider authenticationProvider) {
-        this.jwtAuthFilter = jwtAuthFilter;
-        this.jwtAuthProvider = authenticationProvider;
-    }
+    private final SecurityProperties securityProperties;
+    private final UserDetailsService userDetailsService;
+    private final CounselorRepository counselorRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository)
@@ -35,7 +37,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                        securityProperties.getPermitAllUrls().toArray(new String[0]))
+                        securityProperties.getPermitAllUrls().toArray(String[]::new))
                 .permitAll() // 인증 없이 접근 가능
                 .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
@@ -43,20 +45,13 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않음
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
-                .authenticationProvider(jwtAuthProvider) // 커스텀 AuthenticationProvider 사용
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .headers(headers -> headers
-                .contentSecurityPolicy(csp -> csp
-                .policyDirectives("frame-ancestors 'self'")));
-
-        return http.build();
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))).build();
     }
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomJwtRoleConverter(userDetailsService, counselorRepository));
 
         return jwtAuthenticationConverter;
     }
