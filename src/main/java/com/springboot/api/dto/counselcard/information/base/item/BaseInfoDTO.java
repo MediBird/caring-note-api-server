50dd8ccb51ff00caf5eebb00c549def16342bfe6
Sanghoon <bshjaws1@gmail.com>
2025-01-18 01:38:32 +0900

    refactor: :chart_with_upwards_trend: update BaseInfoDTO

--- src/main/java/com/springboot/api/dto/counselcard/information/base/item/BaseInfoDTO.java
+++ src/main/java/com/springboot/api/dto/counselcard/information/base/item/BaseInfoDTO.java
@@ -2,43 +2,18 @@ package com.springboot.api.dto.counselcard.information.base.item;
 
 import java.time.LocalDate;
 
-<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.springboot.enums.HealthInsuranceType;
 
 import lombok.Builder;
 
-||||||| parent of 2e8ea59 (refactor: :recycle: modify DTOs of the counsel card)
-=======
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.springboot.enums.HealthInsuranceType;
 
-import lombok.Builder;
-
->>>>>>> 2e8ea59 (refactor: :recycle: modify DTOs of the counsel card)
 @Builder
 public record BaseInfoDTO(
         String counseleeId
         ,String name
-<<<<<<< HEAD
         ,@JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthDate
         ,String counselSessionOrder
         ,@JsonFormat(pattern = "yyyy-MM-dd") LocalDate lastCounselDate
         ,HealthInsuranceType healthInsuranceType
-||||||| parent of 2e8ea59 (refactor: :recycle: modify DTOs of the counsel card)
-        , @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthDate
-        , String counselSessionOrder
-        , @JsonFormat(pattern = "yyyy-MM-dd") LocalDate lastCounselDate
-=======
-        ,@JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthDate
-        ,String counselSessionOrder
-        ,@JsonFormat(pattern = "yyyy-MM-dd") LocalDate lastCounselDate
-<<<<<<< HEAD
-        ,MedicalCoverageType medicalCoverage
->>>>>>> 2e8ea59 (refactor: :recycle: modify DTOs of the counsel card)
-||||||| parent of 7db3ffe (fix:)
-        ,MedicalCoverageType medicalCoverage
-=======
-        ,HealthInsuranceType healthInsuranceType
->>>>>>> 7db3ffe (fix:)
 ){}
