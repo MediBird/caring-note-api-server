8be588e491d2c539316cba5df52774f26f621285
Sanghoon <bshjaws1@gmail.com>
2025-01-18 12:50:24 +0900

    feat: :sparkles: add addAndUpdateCounsleee

--- src/main/java/com/springboot/api/controller/CounseleeController.java
+++ src/main/java/com/springboot/api/controller/CounseleeController.java
@@ -3,34 +3,41 @@ package com.springboot.api.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springboot.api.common.annotation.ApiController;
 import com.springboot.api.common.dto.CommonRes;
+import com.springboot.api.dto.counselee.AddAndUpdateCounseleeReq;
 import com.springboot.api.dto.counselee.SelectCounseleeBaseInformationByCounseleeIdRes;
 import com.springboot.api.service.CounseleeService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
-@ApiController(
-        name = "CounseleeController", 
-        description = "내담자 관련 API를 제공하는 Controller", 
-        path = "/v1/counsel/counselee"
-)
+@ApiController(name = "CounseleeController", description = "내담자 관련 API를 제공하는 Controller", path = "/v1/counsel/counselee")
 @RequiredArgsConstructor
 public class CounseleeController {
 
-    private final CounseleeService counseleeService;
+        private final CounseleeService counseleeService;
 
-    @GetMapping("/{counselSessionId}/base/information")
-    @Operation(summary = "내담자 기본 정보 조회", tags = {"상담 카드 작성", "상담 노트"})
-    public ResponseEntity<CommonRes<SelectCounseleeBaseInformationByCounseleeIdRes>> selectCounseleeBaseInformation(
-            @PathVariable("counselSessionId") String counselSessionId) {
+        @GetMapping("/{counselSessionId}/base/information")
+        @Operation(summary = "내담자 기본 정보 조회", tags = { "상담 카드 작성", "상담 노트" })
+        public ResponseEntity<CommonRes<SelectCounseleeBaseInformationByCounseleeIdRes>> selectCounseleeBaseInformation(
+                        @PathVariable("counselSessionId") String counselSessionId) {
 
-        SelectCounseleeBaseInformationByCounseleeIdRes selectCounseleeBaseInformationByCounseleeIdRes = counseleeService
-                .selectCounseleeBaseInformation(counselSessionId);
+                SelectCounseleeBaseInformationByCounseleeIdRes selectCounseleeBaseInformationByCounseleeIdRes = counseleeService
+                                .selectCounseleeBaseInformation(counselSessionId);
 
-        return ResponseEntity.ok(new CommonRes<>(selectCounseleeBaseInformationByCounseleeIdRes));
-    }
+                return ResponseEntity.ok(new CommonRes<>(selectCounseleeBaseInformationByCounseleeIdRes));
+        }
+
+        @PostMapping("/")
+        @Operation(summary = "내담자 기본 정보 생성", tags = { "상담 카드 작성", "상담 노트" })
+        public ResponseEntity<CommonRes<String>> addAndUpdateCounselee(
+                        @RequestBody AddAndUpdateCounseleeReq addAndUpdateCounseleeReq) {
+
+                return ResponseEntity.ok(new CommonRes<>(addAndUpdateCounseleeReq.getCounseleeId()));
+        }
 
 }
