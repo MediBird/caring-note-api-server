3b6cac5975f4ebffddb272c9c4779643e1194c0f
choisunpil <roung4119@gmail.com>
2025-01-21 00:10:49 +0900

    reactor : add MedicationCounselHighlightRepository

---
+++ src/main/java/com/springboot/api/repository/MedicationCounselHighlightRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.api.repository;
+
+import com.springboot.api.domain.MedicationCounselHighlight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicationCounselHighlightRepository extends JpaRepository<MedicationCounselHighlight, String> {
+
+}
