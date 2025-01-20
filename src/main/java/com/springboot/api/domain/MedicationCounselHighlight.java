ae83567687bfceb864cc5850e07646e4564bcc60
choisunpil <roung4119@gmail.com>
2025-01-20 23:21:54 +0900

    reactor : add MedicationCounselHighlight entity

---
+++ src/main/java/com/springboot/api/domain/MedicationCounselHighlight.java
@@ -0,0 +1,33 @@
+package com.springboot.api.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "medication_counsel_highlights")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true, exclude = {"medicationCounsel"})
+@ToString(callSuper = true, exclude = {"medicationCounsel"})
+public class MedicationCounselHighlight extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "medication_counsel_id")
+    private MedicationCounsel medicationCounsel;
+
+    private String highlightRecord;
+    private Integer startIndex;
+    private Integer endIndex;
+
+
+
+
+
+
+}
