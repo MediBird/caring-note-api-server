8dfafd8b80a4916b603c0e0c433d496ea1193cf1
jawsbaek <bshjaws1@gmail.com>
2025-01-29 22:47:51 +0900

    fix: :recycle: remove relationship between counselSession and counseleeConsent (#86)

--- src/main/java/com/springboot/api/domain/CounseleeConsent.java
+++ src/main/java/com/springboot/api/domain/CounseleeConsent.java
@@ -1,6 +1,5 @@
 package com.springboot.api.domain;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -9,22 +8,18 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "counselee_consents", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"counsel_session_id", "counselee_id"})
+        @UniqueConstraint(columnNames = { "counselee_id" })
 })
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true, exclude = {"counselSession", "counselee",})
-@ToString(callSuper = true, exclude = {"counselSession", "counselee"})
+@EqualsAndHashCode(callSuper = true, exclude = { "counselee" })
+@ToString(callSuper = true, exclude = { "counselee" })
 public class CounseleeConsent extends BaseEntity {
 
-    @OneToOne
-    @JoinColumn(name = "counsel_session_id", nullable = false)
-    private CounselSession counselSession;
-
     @ManyToOne
-    @JoinColumn(name ="counselee_id", nullable = false)
+    @JoinColumn(name = "counselee_id", nullable = false)
     private Counselee counselee;
 
     @NotNull
