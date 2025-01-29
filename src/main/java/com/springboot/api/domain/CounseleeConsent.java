d49524ad1fb15532b6cfcdce7aed69724d84b07b
Sanghoon <bshjaws1@gmail.com>
2025-01-29 23:12:24 +0900

    fix: :sparkles: fix CounseleeConsent

--- src/main/java/com/springboot/api/domain/CounseleeConsent.java
+++ src/main/java/com/springboot/api/domain/CounseleeConsent.java
@@ -3,23 +3,26 @@ package com.springboot.api.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "counselee_consents", uniqueConstraints = {
-        @UniqueConstraint(columnNames = { "counselee_id" })
+        @UniqueConstraint(columnNames = { "counsel_session_id", "counselee_id" })
 })
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true, exclude = { "counselee" })
-@ToString(callSuper = true, exclude = { "counselee" })
+@EqualsAndHashCode(callSuper = true, exclude = { "counselSession", "counselee", })
+@ToString(callSuper = true, exclude = { "counselSession", "counselee" })
 public class CounseleeConsent extends BaseEntity {
 
-    @ManyToOne
-    @JoinColumn(name = "counselee_id", nullable = false)
+    @OneToOne
+    @JoinColumn(name = "counselee_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Counselee counselee;
 
     @NotNull
