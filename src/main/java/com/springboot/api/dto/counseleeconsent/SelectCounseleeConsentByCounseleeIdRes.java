a2a65ad4b946004bb873a3bce1f9939a91292be7
jawsbaek <bshjaws1@gmail.com>
2025-01-30 00:37:03 +0900

    Revert "fix: :recycle: refactor CounseleeConsentController (#87)"
    This reverts commit e923c4c158655dd3cca70589622ee430f62ab691.

--- src/main/java/com/springboot/api/dto/counseleeconsent/SelectCounseleeConsentByCounseleeIdRes.java
+++ src/main/java/com/springboot/api/dto/counseleeconsent/SelectCounseleeConsentByCounseleeIdRes.java
@@ -3,6 +3,10 @@ package com.springboot.api.dto.counseleeconsent;
 import java.time.LocalDateTime;
 
 public record SelectCounseleeConsentByCounseleeIdRes(
-                String counseleeConsentId, String counseleeId, String counseleeName, LocalDateTime consentDateTime,
-                boolean isConsent) {
-}
+        String counseleeConsentId
+        , String counseleeId
+        , String counseleeName
+        , String counselSessionId
+        , LocalDateTime consentDateTime
+        , boolean isConsent
+){}
