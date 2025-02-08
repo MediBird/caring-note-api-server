d1a104039ae8d56bc5ebf240cb76d8ed59c08bf6
choisunpil <roung4119@gmail.com>
2025-02-08 21:06:47 +0900

    refactor : add Constructor Annotation

--- src/main/java/com/springboot/api/dto/naverClova/DiarizationDTO.java
+++ src/main/java/com/springboot/api/dto/naverClova/DiarizationDTO.java
@@ -1,10 +1,14 @@
 package com.springboot.api.dto.naverClova;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DiarizationDTO {
     @Builder.Default
     private final Boolean enable = Boolean.FALSE;
