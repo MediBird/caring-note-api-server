6d23cff9b34f472f01b8ed537487ede2e745039c
Sanghoon <bshjaws1@gmail.com>
2025-02-16 02:11:32 +0900

    fix: :safety_vest: fix validation errors

--- src/main/java/com/springboot/api/dto/wasteMedication/AddAndUpdateWasteMedicationRecordReq.java
+++ src/main/java/com/springboot/api/dto/wasteMedication/AddAndUpdateWasteMedicationRecordReq.java
@@ -1,5 +1,6 @@
 package com.springboot.api.dto.wasteMedication;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -7,13 +8,13 @@ import lombok.Data;
 @Data
 public class AddAndUpdateWasteMedicationRecordReq {
 
+    @Nullable
     private String rowId;
 
     @NotBlank(message = "약물 ID는 필수 입력값입니다")
     @Size(min = 26, max = 26, message = "약물 ID는 26자여야 합니다")
     private String medicationId;
 
-    @NotBlank(message = "약물 단위는 필수 입력값입니다")
     private Integer unit;
 
     @NotBlank(message = "약물 폐기 이유는 필수 입력값입니다")
