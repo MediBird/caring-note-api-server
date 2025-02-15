d5825391f5064b276e70593899b65ed4db6e55a9
jawsbaek <bshjaws1@gmail.com>
2025-02-16 01:42:57 +0900

    fix: :safety_vest: (#110)

--- src/main/java/com/springboot/api/dto/medicationcounsel/DeleteMedicationCounselReq.java
+++ src/main/java/com/springboot/api/dto/medicationcounsel/DeleteMedicationCounselReq.java
@@ -1,7 +1,5 @@
 package com.springboot.api.dto.medicationcounsel;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -14,9 +12,4 @@ public class DeleteMedicationCounselReq {
     @NotBlank(message = "상담 노트 ID는 필수 입력값입니다")
     @Size(min = 26, max = 26, message = "상담 노트 ID는 26자여야 합니다")
     private String medicationCounselId;
-
-    @JsonCreator
-    public DeleteMedicationCounselReq(@JsonProperty("medicationCounselId") String medicationCounselId) {
-        this.medicationCounselId = medicationCounselId;
-    }
 }
