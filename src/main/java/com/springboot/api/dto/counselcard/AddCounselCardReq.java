25bc8da5aacb5d5e812c5c93372bcd216b4971c0
Sanghoon <bshjaws1@gmail.com>
2025-02-16 01:31:57 +0900

    feat: :safety_vest:

--- src/main/java/com/springboot/api/dto/counselcard/AddCounselCardReq.java
+++ src/main/java/com/springboot/api/dto/counselcard/AddCounselCardReq.java
@@ -8,6 +8,7 @@ import com.springboot.api.dto.counselcard.information.living.LivingInformationDT
 import com.springboot.enums.CardRecordStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,7 +16,8 @@ import lombok.Data;
 @Builder
 public class AddCounselCardReq {
 
-    @NotBlank
+    @NotBlank(message = "상담 세션 ID는 필수 입력값입니다")
+    @Size(min = 26, max = 26, message = "상담 세션 ID는 26자여야 합니다")
     private String counselSessionId;
 
     @ValidEnum(enumClass = CardRecordStatus.class)
--- src/main/java/com/springboot/api/dto/counselcard/DeleteCounselCardReq.java
+++ src/main/java/com/springboot/api/dto/counselcard/DeleteCounselCardReq.java
@@ -1,6 +1,7 @@
 package com.springboot.api.dto.counselcard;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import lombok.Data;
 @Builder
 public class DeleteCounselCardReq {
 
-    @NotBlank
+    @NotBlank(message = "상담카드 ID는 필수 입력값입니다")
+    @Size(min = 26, max = 26, message = "상담카드 ID는 26자여야 합니다")
     private String counselCardId;
 }
