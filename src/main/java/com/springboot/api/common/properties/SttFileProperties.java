7dddf0f32a190dd8f1a3da106d1f8cbb11f1bdf1
choisunpil <roung4119@gmail.com>
2025-02-16 23:22:40 +0900

    fix : properties prefix

--- src/main/java/com/springboot/api/common/properties/SttFileProperties.java
+++ src/main/java/com/springboot/api/common/properties/SttFileProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix="naver.clova")
+@ConfigurationProperties(prefix="stt.file.path")
 @Getter
 @Setter
 public class SttFileProperties {
