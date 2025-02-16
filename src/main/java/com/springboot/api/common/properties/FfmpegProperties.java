b1ebcfb35c6544e4e86732551eb4f22efd54c992
SunPil Choi <33895647+ChoiSunPil@users.noreply.github.com>
2025-02-16 23:23:33 +0900

    fix : properties prefix (#118)

--- src/main/java/com/springboot/api/common/properties/FfmpegProperties.java
+++ src/main/java/com/springboot/api/common/properties/FfmpegProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix="stt.file.path")
+@ConfigurationProperties(prefix="ffmpeg")
 @Getter
 @Setter
 public class FfmpegProperties {
