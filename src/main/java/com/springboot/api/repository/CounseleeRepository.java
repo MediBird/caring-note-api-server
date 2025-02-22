71d69ae62939bf6134b7b73e2fa2a93117bd1157
jawsbaek <bshjaws1@gmail.com>
2025-02-22 22:24:58 +0900

    feat: :sparkles: fix sql for filters (#125)

--- src/main/java/com/springboot/api/repository/CounseleeRepository.java
+++ src/main/java/com/springboot/api/repository/CounseleeRepository.java
@@ -15,20 +15,18 @@ public interface CounseleeRepository extends JpaRepository<Counselee, String> {
         @Query(value = """
                             SELECT * FROM counselees c
                             WHERE (:name IS NULL OR c.name ILIKE CONCAT('%', :name, '%'))
-                            AND (CAST(:birthDates AS date[]) IS NULL OR ARRAY_LENGTH(CAST(:birthDates AS date[]), 1) IS NULL
-                                OR c.date_of_birth = ANY(CAST(:birthDates AS date[])))
-                            AND (CAST(:affiliatedWelfareInstitutions AS varchar[]) IS NULL
-                                OR ARRAY_LENGTH(CAST(:affiliatedWelfareInstitutions AS varchar[]), 1) IS NULL
-                                OR c.affiliated_welfare_institution = ANY(CAST(:affiliatedWelfareInstitutions AS varchar[])))
+                            AND (:birthDates IS NULL OR CARDINALITY(:birthDates) = 0
+                                OR c.date_of_birth = ANY(:birthDates))
+                            AND (:affiliatedWelfareInstitutions IS NULL OR CARDINALITY(:affiliatedWelfareInstitutions) = 0
+                                OR c.affiliated_welfare_institution = ANY(:affiliatedWelfareInstitutions))
                             ORDER BY c.registration_date DESC
                         """, countQuery = """
                             SELECT COUNT(*) FROM counselees c
                             WHERE (:name IS NULL OR c.name ILIKE CONCAT('%', :name, '%'))
-                            AND (CAST(:birthDates AS date[]) IS NULL OR ARRAY_LENGTH(CAST(:birthDates AS date[]), 1) IS NULL
-                                OR c.date_of_birth = ANY(CAST(:birthDates AS date[])))
-                            AND (CAST(:affiliatedWelfareInstitutions AS varchar[]) IS NULL
-                                OR ARRAY_LENGTH(CAST(:affiliatedWelfareInstitutions AS varchar[]), 1) IS NULL
-                                OR c.affiliated_welfare_institution = ANY(CAST(:affiliatedWelfareInstitutions AS varchar[])))
+                            AND (:birthDates IS NULL OR CARDINALITY(:birthDates) = 0
+                                OR c.date_of_birth = ANY(:birthDates))
+                            AND (:affiliatedWelfareInstitutions IS NULL OR CARDINALITY(:affiliatedWelfareInstitutions) = 0
+                                OR c.affiliated_welfare_institution = ANY(:affiliatedWelfareInstitutions))
                         """, nativeQuery = true)
         Page<Counselee> findWithFilters(
                         @Param("name") String name,
