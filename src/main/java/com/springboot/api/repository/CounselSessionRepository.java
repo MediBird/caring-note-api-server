8e38412ba926a4ed84fc80040da97db30815c1c6
Sanghoon <bshjaws1@gmail.com>
2025-02-23 23:40:10 +0900

    refactor: :recycle: use springJpa respository

--- src/main/java/com/springboot/api/repository/CounselSessionRepository.java
+++ src/main/java/com/springboot/api/repository/CounselSessionRepository.java
@@ -10,48 +10,52 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
 import com.springboot.api.domain.CounselSession;
+import com.springboot.enums.ScheduleStatus;
 
 public interface CounselSessionRepository
-                extends JpaRepository<CounselSession, String>, CounselSessionRepositoryCustom {
+    extends JpaRepository<CounselSession, String>, CounselSessionRepositoryCustom {
 
-        @Override
-        void deleteById(@NonNull String id);
+  @Override
+  void deleteById(@NonNull String id);
 
-        @Query("""
-                        SELECT cs FROM CounselSession cs
-                        WHERE (cs.scheduledStartDateTime >= :startOfDay)
-                          AND (cs.scheduledStartDateTime < :endOfDay)
-                          AND (:cursorId IS NULL OR cs.id > :cursorId)
-                          AND (:counselorId IS NULL OR cs.counselor.id = :counselorId)
-                        ORDER BY cs.id ASC
-                        """)
-        List<CounselSession> findByDateAndCursor(
-                        @Param("startOfDay") LocalDateTime startOfDay,
-                        @Param("endOfDay") LocalDateTime endOfDay,
-                        @Param("cursorId") String cursorId,
-                        @Param("counselorId") String counselorId,
-                        Pageable pageable);
+  @Query("""
+      SELECT cs FROM CounselSession cs
+      WHERE (cs.scheduledStartDateTime >= :startOfDay)
+        AND (cs.scheduledStartDateTime < :endOfDay)
+        AND (:cursorId IS NULL OR cs.id > :cursorId)
+        AND (:counselorId IS NULL OR cs.counselor.id = :counselorId)
+      ORDER BY cs.id ASC
+      """)
+  List<CounselSession> findByDateAndCursor(
+      @Param("startOfDay") LocalDateTime startOfDay,
+      @Param("endOfDay") LocalDateTime endOfDay,
+      @Param("cursorId") String cursorId,
+      @Param("counselorId") String counselorId,
+      Pageable pageable);
 
-        @Query("""
-                        SELECT cs FROM CounselSession cs
-                        WHERE (:cursorId IS NULL OR cs.id > :cursorId)
-                          AND (:counselorId IS NULL OR cs.counselor.id = :counselorId)
-                        ORDER BY cs.id ASC
-                        """)
-        List<CounselSession> findByCursor(
-                        @Param("cursorId") String cursorId,
-                        @Param("counselorId") String counselorId,
-                        Pageable pageable);
+  @Query("""
+      SELECT cs FROM CounselSession cs
+      WHERE (:cursorId IS NULL OR cs.id > :cursorId)
+        AND (:counselorId IS NULL OR cs.counselor.id = :counselorId)
+      ORDER BY cs.id ASC
+      """)
+  List<CounselSession> findByCursor(
+      @Param("cursorId") String cursorId,
+      @Param("counselorId") String counselorId,
+      Pageable pageable);
 
-        @Query("""
-                        SELECT cs FROM CounselSession cs
-                        WHERE cs.counselee.id = :counseleeId
-                         AND cs.id < :id
-                        ORDER BY cs.id DESC
-                        """)
-        List<CounselSession> findByCounseleeIdPrevious(
-                        @Param("counseleeId") String counseleeId, @Param("id") String id, Pageable pageable);
+  @Query("""
+      SELECT cs FROM CounselSession cs
+      WHERE cs.counselee.id = :counseleeId
+       AND cs.id < :id
+      ORDER BY cs.id DESC
+      """)
+  List<CounselSession> findByCounseleeIdPrevious(
+      @Param("counseleeId") String counseleeId, @Param("id") String id, Pageable pageable);
 
-        List<CounselSession> findByCounseleeIdAndScheduledStartDateTimeLessThan(String counseleeId,
-                        LocalDateTime scheduledStartDateTime);
+  List<CounselSession> findByCounseleeIdAndScheduledStartDateTimeLessThan(String counseleeId,
+      LocalDateTime scheduledStartDateTime);
+
+  List<CounselSession> findByStatusAndScheduledStartDateTimeBefore(ScheduleStatus status,
+      LocalDateTime dateTime);
 }
