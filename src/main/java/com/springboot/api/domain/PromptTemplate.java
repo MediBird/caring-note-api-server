f393216d5d89ce3cdac3e1776e1f5cc10f38ae25
choisunpil <roung4119@gmail.com>
2025-02-23 12:30:37 +0900

    refactor : prompt string -> db  in analyseText

--- src/main/java/com/springboot/api/domain/PromptTemplate.java
+++ src/main/java/com/springboot/api/domain/PromptTemplate.java
@@ -3,7 +3,15 @@ package com.springboot.api.domain;
 import com.springboot.enums.PromptTemplateType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -16,6 +24,7 @@ import java.util.List;
 @ToString(callSuper = true, exclude = {"promptLearnings"})
 public class PromptTemplate extends BaseEntity{
 
+    private static final Logger log = LoggerFactory.getLogger(PromptTemplate.class);
     private String name;
 
     private String description;
@@ -35,6 +44,38 @@ public class PromptTemplate extends BaseEntity{
     @Override
     protected void onCreate() {
         super.onCreate();
+    }
+
+    public List<Message> generatePromptMessages(UserMessage userMessage) {
+
+        ArrayList<Message> messages = new ArrayList<>();
+
+        if(PromptTemplateType.SYSTEM.equals(this.promptTemplateType)) {
+            messages.add(new SystemMessage(promptText));
+        }
+        else{
+            messages.add(new UserMessage(promptText));
+        }
+
+        for(PromptLearning promptLearning : promptLearnings) {
+            StringBuilder learingStringBuilder = new StringBuilder();
+
+            if (!StringUtils.isBlank(promptLearning.getLearningInputText())) {
+                learingStringBuilder.append("input: \n");
+                learingStringBuilder.append(promptLearning.getLearningInputText());
+            }
+            if(!StringUtils.isBlank(promptLearning.getLearningOutputText()))
+            {
+                learingStringBuilder.append("output: \n");
+                learingStringBuilder.append(promptLearning.getLearningOutputText());
+            }
+            messages.add(new AssistantMessage(learingStringBuilder.toString()));
+        }
+
+        messages.add(userMessage);
+
+        log.info(messages.toString());
+        return messages;
     }
 
 }
