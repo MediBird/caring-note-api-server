d175e0e2922640e57f377c76ad406341ace9caa7
rappire <rappire@gmail.com>
2025-02-27 00:23:59 +0900

    fix: fix bug on DeleteCounselSessionReq

--- src/main/java/com/springboot/api/dto/counselsession/DeleteCounselSessionReq.java
+++ src/main/java/com/springboot/api/dto/counselsession/DeleteCounselSessionReq.java
@@ -2,11 +2,9 @@ package com.springboot.api.dto.counselsession;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class DeleteCounselSessionReq {
     @NotBlank(message = "상담 세션 ID는 필수 입력값입니다")
     @Size(min = 26, max = 26, message = "상담 세션 ID는 26자여야 합니다")
