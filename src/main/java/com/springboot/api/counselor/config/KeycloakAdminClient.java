1718c2229b52bb4508d53616d6c2204f89fa571c
jawsbaek <bshjaws1@gmail.com>
2025-03-12 16:36:22 +0900

    feat: :sparkles: add keycloak admin client library (#151)

---
+++ src/main/java/com/springboot/api/counselor/config/KeycloakAdminClient.java
@@ -0,0 +1,38 @@
+package com.springboot.api.counselor.config;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "keycloak")
+@Data
+public class KeycloakAdminClient {
+
+    private String url;
+    private String realm;
+    private String adminUsername;
+    private String adminPassword;
+    private final String clientId = "admin-cli";
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(url)
+                .realm("master")
+                .username(adminUsername)
+                .password(adminPassword)
+                .clientId(clientId)
+                .build();
+    }
+
+    @Bean
+    public RealmResource realmResource() {
+        return keycloak().realm(realm);
+    }
+}
