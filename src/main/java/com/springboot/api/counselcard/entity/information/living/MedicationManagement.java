79b7c725ed5ee81d7b8774997cea88f3f1416381
Sanghoon <bshjaws1@gmail.com>
2025-03-16 23:35:30 +0900

    refactor: :recycle: refactor MedicationManagement

--- src/main/java/com/springboot/api/counselcard/entity/information/living/MedicationManagement.java
+++ src/main/java/com/springboot/api/counselcard/entity/information/living/MedicationManagement.java
@@ -1,8 +1,15 @@
 package com.springboot.api.counselcard.entity.information.living;
 
-import com.springboot.api.counselcard.dto.information.living.MedicationManagementDTO;
-import jakarta.persistence.Embeddable;
 import java.util.Objects;
+import java.util.Set;
+
+import com.springboot.api.counselcard.dto.information.living.MedicationManagementDTO;
+import com.springboot.enums.MedicationAssistant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,21 +17,35 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MedicationManagement {
-    private Boolean isMedicationManagement;
+    private Boolean isAlone;
+
+    @Column(nullable = false)
+    private String houseMateNote;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Set<MedicationAssistant> medicationAssistants;
 
     public static MedicationManagement initializeDefault() {
         MedicationManagement medicationManagement = new MedicationManagement();
-        medicationManagement.isMedicationManagement = false;
+        medicationManagement.isAlone = false;
+        medicationManagement.houseMateNote = "";
+        medicationManagement.medicationAssistants = Set.of();
         return medicationManagement;
     }
 
     public static MedicationManagement copy(MedicationManagement medicationManagement) {
         MedicationManagement copiedMedicationManagement = new MedicationManagement();
-        copiedMedicationManagement.isMedicationManagement = medicationManagement.isMedicationManagement;
+        copiedMedicationManagement.isAlone = medicationManagement.isAlone;
+        copiedMedicationManagement.houseMateNote = medicationManagement.houseMateNote;
+        copiedMedicationManagement.medicationAssistants = Set.copyOf(medicationManagement.medicationAssistants);
         return copiedMedicationManagement;
     }
 
     public void update(MedicationManagementDTO medicationManagementDTO) {
-        this.isMedicationManagement = Objects.requireNonNullElse(medicationManagementDTO.isMedicationManagement(), this.isMedicationManagement);
+        this.isAlone = Objects.requireNonNullElse(medicationManagementDTO.isAlone(), this.isAlone);
+        this.houseMateNote = Objects.requireNonNullElse(medicationManagementDTO.houseMateNote(), this.houseMateNote);
+        this.medicationAssistants = Objects.requireNonNullElse(medicationManagementDTO.medicationAssistants(),
+                this.medicationAssistants);
     }
 }
