5e8ae1f31b87bf0573cbf5bfbaba2dcd71afd1a1
jawsbaek <bshjaws1@gmail.com>
2025-03-16 23:40:49 +0900

    refactor: :recycle: refactor MedicationManagement (#170)

--- src/main/java/com/springboot/api/counselcard/dto/information/living/MedicationManagementDTO.java
+++ src/main/java/com/springboot/api/counselcard/dto/information/living/MedicationManagementDTO.java
@@ -1,13 +1,20 @@
 package com.springboot.api.counselcard.dto.information.living;
 
+import java.util.Set;
+
+import com.springboot.api.common.annotation.ValidEnum;
 import com.springboot.api.counselcard.entity.information.living.MedicationManagement;
+import com.springboot.enums.MedicationAssistant;
 
 public record MedicationManagementDTO(
-        Boolean isMedicationManagement) {
+        Boolean isAlone,
+        String houseMateNote,
+        @ValidEnum(enumClass = MedicationAssistant.class) Set<MedicationAssistant> medicationAssistants) {
 
-    public MedicationManagementDTO (MedicationManagement medicationManagement){
+    public MedicationManagementDTO(MedicationManagement medicationManagement) {
         this(
-            medicationManagement.getIsMedicationManagement()
-        );
+                medicationManagement.getIsAlone(),
+                medicationManagement.getHouseMateNote(),
+                medicationManagement.getMedicationAssistants());
     }
 }
