88c600691221ca129ef48f1e22c1edac96e57fa5
Sanghoon <bshjaws1@gmail.com>
2025-03-17 02:30:15 +0900

    refactor: :recycle: refactor set to list

--- src/main/java/com/springboot/api/counselcard/dto/information/independentlife/CommunicationDTO.java
+++ src/main/java/com/springboot/api/counselcard/dto/information/independentlife/CommunicationDTO.java
@@ -6,13 +6,13 @@ import com.springboot.enums.CommunicationType;
 import com.springboot.enums.HearingType;
 import com.springboot.enums.SightType;
 import com.springboot.enums.UsingKoreanType;
-import java.util.Set;
+import java.util.List;
 
 public record CommunicationDTO(
-    @ValidEnum(enumClass = SightType.class) Set<SightType> sights,
-    @ValidEnum(enumClass = HearingType.class) Set<HearingType> hearings,
+    @ValidEnum(enumClass = SightType.class) List<SightType> sights,
+    @ValidEnum(enumClass = HearingType.class) List<HearingType> hearings,
     @ValidEnum(enumClass = CommunicationType.class) CommunicationType communications,
-    @ValidEnum(enumClass = UsingKoreanType.class) Set<UsingKoreanType> usingKoreans) {
+    @ValidEnum(enumClass = UsingKoreanType.class) List<UsingKoreanType> usingKoreans) {
 
     public CommunicationDTO(Communication communication) {
         this(
