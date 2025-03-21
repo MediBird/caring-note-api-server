2e032f29a485f2a85ce6e38d32b78610bffe24bf
Sanghoon <bshjaws1@gmail.com>
2025-03-21 10:56:54 +0900

    refactor: :recycle: refactor AllergyDTO

--- src/main/java/com/springboot/api/counselcard/dto/information/health/AllergyDTO.java
+++ src/main/java/com/springboot/api/counselcard/dto/information/health/AllergyDTO.java
@@ -3,9 +3,10 @@ package com.springboot.api.counselcard.dto.information.health;
 import com.springboot.api.counselcard.entity.information.health.Allergy;
 
 public record AllergyDTO(
+    boolean isAllergic,
     String allergyNote) {
 
     public AllergyDTO(Allergy allergy) {
-        this(allergy.getAllergyNote());
+        this(allergy.isAllergic(), allergy.getAllergyNote());
     }
 }
--- src/main/java/com/springboot/api/counselcard/entity/information/health/Allergy.java
+++ src/main/java/com/springboot/api/counselcard/entity/information/health/Allergy.java
@@ -1,9 +1,11 @@
 package com.springboot.api.counselcard.entity.information.health;
 
+import java.util.Objects;
+
 import com.springboot.api.counselcard.dto.information.health.AllergyDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,20 +16,26 @@ public class Allergy {
     @Column(nullable = false)
     private String allergyNote;
 
+    @Column(nullable = false)
+    private boolean isAllergic;
+
     public static Allergy initializeDefault() {
         Allergy allergy = new Allergy();
         allergy.allergyNote = "";
+        allergy.isAllergic = false;
         return allergy;
     }
 
     public static Allergy copy(Allergy allergy) {
         Allergy copiedAllergy = new Allergy();
         copiedAllergy.allergyNote = allergy.allergyNote;
+        copiedAllergy.isAllergic = allergy.isAllergic;
         return copiedAllergy;
     }
 
     public void update(AllergyDTO allergyDTO) {
         this.allergyNote = Objects.requireNonNullElse(allergyDTO.allergyNote(), this.allergyNote);
+        this.isAllergic = Objects.requireNonNullElse(allergyDTO.isAllergic(), this.isAllergic);
     }
 }
 
