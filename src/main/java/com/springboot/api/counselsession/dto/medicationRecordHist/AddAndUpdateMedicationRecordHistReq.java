844495b87cc1c9c3db671f9cacfbb316be42a213
Sanghoon <bshjaws1@gmail.com>
2025-03-29 21:54:07 +0900

    refactor: :recycle: refactor AddAndUpdateMedicationRecordHistReq

--- src/main/java/com/springboot/api/counselsession/dto/medicationRecordHist/AddAndUpdateMedicationRecordHistReq.java
+++ src/main/java/com/springboot/api/counselsession/dto/medicationRecordHist/AddAndUpdateMedicationRecordHistReq.java
@@ -5,9 +5,7 @@ import com.springboot.enums.MedicationDivision;
 import com.springboot.enums.MedicationUsageStatus;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -16,27 +14,26 @@ public class AddAndUpdateMedicationRecordHistReq {
     @Nullable
     private String rowId;
 
-    @NotBlank(message = "약물 ID는 필수 입력값입니다")
+    @Nullable
     @Size(min = 26, max = 26, message = "약물 ID는 26자여야 합니다")
     private String medicationId;
 
     @ValidEnum(enumClass = MedicationDivision.class)
     private MedicationDivision divisionCode;
 
-    @NotBlank(message = "처방일자는 필수 입력값입니다")
+    @Nullable
     private String prescriptionDate;
 
-    @NotNull(message = "처방일수는 필수 입력값입니다")
-    @Min(value = 1, message = "처방일수는 1일 이상이어야 합니다")
+    @Nullable
     private int prescriptionDays;
 
     @NotBlank(message = "약물 이름은 필수 입력값입니다")
     private String medicationName;
 
-    @NotBlank(message = "약물 사용 대상은 필수 입력값입니다")
+    @Nullable
     private String usageObject;
 
-    @NotBlank(message = "약물 단위는 필수 입력값입니다")
+    @Nullable
     private String unit;
 
     @ValidEnum(enumClass = MedicationUsageStatus.class)
