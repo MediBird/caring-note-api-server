5d17ed990ef4a4fbd141516bd1c78090d752ea47
Sanghoon <bshjaws1@gmail.com>
2025-04-08 23:42:40 +0900

    refactor: :recycle: refactor medication services

--- src/main/java/com/springboot/api/medication/service/MedicationService.java
+++ src/main/java/com/springboot/api/medication/service/MedicationService.java
@@ -22,39 +22,19 @@ public class MedicationService {
 
     @Cacheable(value = "medicationSearch")
     public List<SearchMedicationByKeywordRes> searchMedicationByKeyword(String keyword) {
-        log.info("keyword: {}", keyword);
-        return medicationRepository.searchByItemNameWithPattern(
-                keyword,
-                createSearchPattern(keyword))
+        log.info("검색 키워드: {}", keyword);
+
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return List.of();
+        }
+
+        return medicationRepository.searchByKeyword(keyword.trim())
                 .stream()
-                .limit(10)
                 .map(medication -> SearchMedicationByKeywordRes.builder()
                         .id(medication.getId())
                         .itemName(medication.getItemName())
                         .itemImage(medication.getItemImage())
                         .build())
                 .collect(Collectors.toList());
-    }
-
-    public String createSearchPattern(String keyword) {
-        return convertToChosung(keyword);
-    }
-
-    public String convertToChosung(String text) {
-        String[] chosung = { "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ",
-                "ㅎ" };
-        StringBuilder result = new StringBuilder();
-
-        for (char ch : text.toCharArray()) {
-            if (ch >= '가' && ch <= '힣') {
-                int unicode = ch - '가';
-                int cho = unicode / (21 * 28);
-                result.append(chosung[cho]);
-            } else {
-                result.append(ch);
-            }
-        }
-        log.info(result.toString());
-        return result.toString();
     }
 }
