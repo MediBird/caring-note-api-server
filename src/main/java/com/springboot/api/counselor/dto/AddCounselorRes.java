a1d745f07e965b7c18658f084a52f978b4749e11
rappire <rappire@gmail.com>
2025-04-27 01:09:05 +0900

    style: :art: remove useless dto

--- src/main/java/com/springboot/api/counselor/dto/AddCounselorRes.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.api.counselor.dto;
-
-import com.springboot.enums.RoleType;
-
-public record AddCounselorRes(String id, RoleType roleType) {
-
-}
--- src/main/java/com/springboot/api/counselor/dto/LoginCounselorRes.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.api.counselor.dto;
-
-import com.springboot.enums.RoleType;
-
-public record LoginCounselorRes(String counselorId, RoleType roleType) {
-
-}
