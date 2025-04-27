eb611d0ac3bf0993dd9eac7ccc7f60bbdd54ff3c
jawsbaek <bshjaw1@gmail.com>
2025-04-27 21:19:49 +0900

    revert: :fire: remove plugin for cursor environment

--- src/test/java/com/springboot/api/service/AICounselSummaryServiceTest.java
+++ src/test/java/com/springboot/api/service/AICounselSummaryServiceTest.java
@@ -23,7 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
