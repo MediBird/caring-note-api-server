fc7c08377deac5a4ef18762cb60f1b59f465ffcd
jawsbaek <bshjaws1@gmail.com>
2025-05-15 21:16:12 +0900

    refactor: :recycle: refactor tus service (#241)

--- src/main/java/com/springboot/api/tus/dto/response/TusFileInfoRes.java
+++ src/main/java/com/springboot/api/tus/dto/response/TusFileInfoRes.java
@@ -1,20 +1,27 @@
 package com.springboot.api.tus.dto.response;
 
 import com.springboot.api.tus.entity.TusFileInfo;
+
 import lombok.Getter;
 
 @Getter
 public class TusFileInfoRes {
 
-    private final String location;
+    private final String fileId;
+    private final String originalFilename;
     private final Long contentLength;
     private final Long contentOffset;
     private final Boolean isDefer;
+    private final String location;
+    private final Long duration;
 
     public TusFileInfoRes(TusFileInfo tusFileInfo, String location) {
-        this.location = location;
+        this.fileId = tusFileInfo.getId();
+        this.originalFilename = tusFileInfo.getOriginalName();
         this.contentLength = tusFileInfo.getContentLength();
         this.contentOffset = tusFileInfo.getContentOffset();
         this.isDefer = tusFileInfo.getIsDefer();
+        this.location = location;
+        this.duration = tusFileInfo.getDuration();
     }
 }
