b9108fb440b1997fdf4ad648109d5e112df53b43
rappire <rappire@gmail.com>
2025-05-22 23:11:25 +0900

    refactor: :recycle: update Counselor and SelectCounselSessionRes for improved structure and readability

--- src/main/java/com/springboot/api/counselsession/dto/counselsession/SelectCounselSessionRes.java
+++ src/main/java/com/springboot/api/counselsession/dto/counselsession/SelectCounselSessionRes.java
@@ -3,36 +3,40 @@ package com.springboot.api.counselsession.dto.counselsession;
 import com.springboot.api.counselor.entity.Counselor;
 import com.springboot.api.counselsession.entity.CounselSession;
 import com.springboot.enums.ScheduleStatus;
-import java.util.Optional;
-import lombok.Builder;
+import lombok.Getter;
 
-@Builder
-public record SelectCounselSessionRes(
-    String counselSessionId,
-    String scheduledTime,
-    String scheduledDate,
-    String counseleeId,
-    String counseleeName,
-    String counselorId,
-    String counselorName,
-    Integer sessionNumber,
-    ScheduleStatus status) {
+@Getter
+public class SelectCounselSessionRes {
+
+    private String counselSessionId;
+    private String scheduledTime;
+    private String scheduledDate;
+    private String counseleeId;
+    private String counseleeName;
+    private String counselorId;
+    private String counselorName;
+    private String sessionNumber;
+    private ScheduleStatus status;
 
     public static SelectCounselSessionRes from(CounselSession counselSession) {
-        return SelectCounselSessionRes.builder()
-            .counselSessionId(counselSession.getId())
-            .scheduledTime(counselSession.getScheduledStartDateTime().toLocalTime().toString())
-            .scheduledDate(counselSession.getScheduledStartDateTime().toLocalDate().toString())
-            .counseleeId(counselSession.getCounselee().getId())
-            .counseleeName(counselSession.getCounselee().getName())
-            .counselorId(Optional.ofNullable(counselSession.getCounselor())
-                .map(Counselor::getId)
-                .orElse(""))
-            .counselorName(Optional.ofNullable(counselSession.getCounselor())
-                .map(Counselor::getName)
-                .orElse(""))
-            .sessionNumber(counselSession.getSessionNumber())
-            .status(counselSession.getStatus())
-            .build();
+        SelectCounselSessionRes res = new SelectCounselSessionRes();
+
+        res.counselSessionId = counselSession.getId();
+        res.scheduledTime = counselSession.getScheduledStartDateTime().toLocalTime().toString();
+        res.scheduledDate = counselSession.getScheduledStartDateTime().toLocalDate().toString();
+
+        res.counseleeId = counselSession.getCounselee().getId();
+        res.counseleeName = counselSession.getCounselee().getName();
+
+        Counselor counselor = counselSession.getCounselor();
+        res.counselorId = (counselor != null) ? counselor.getId() : "";
+        res.counselorName = (counselor != null) ? counselor.getName() : "";
+
+        res.status = counselSession.getStatus();
+        res.sessionNumber = res.status.equals(ScheduleStatus.CANCELED) ? "-" :
+            String.valueOf(counselSession.getSessionNumber());
+
+        return res;
     }
+
 }
